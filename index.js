4a12291e05ac67708660d3cf82c3a7d5c31852d8
EnZon3 <37979038+EnZon3@users.noreply.github.com>
2022-08-26 19:10:00 -0400

    cleaning

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-//express boilerplate
 var express = require('express');
 var app = express();
+const PORT = 8080;
 
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
     res.send('Hello World');
 });
+
+app.listen(
+    PORT,
+    () => console.log(`running on port ${PORT}`)
+)
